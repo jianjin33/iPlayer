b00de06dc6cd3dd055ee61e5082d767c72a9ccb8
jianjin <jianjin33@gmail.com>
2017-09-20 16:36:55 +0800

    各个module中ButterKnife库引用

--- module_main/src/main/java/com/iplayer/main/ui/Test2Activity.java
+++ module_main/src/main/java/com/iplayer/main/ui/Test2Activity.java
@@ -2,16 +2,27 @@ package com.iplayer.main.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.iplayer.annotation.router.AutoRouter;
 import com.iplayer.main.R;
+import com.iplayer.main.R2;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 @AutoRouter()
 public class Test2Activity extends AppCompatActivity {
 
+    @BindView(R2.id.test_text)
+    TextView text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_test2);
+        ButterKnife.bind(this);
+        text.setText("hahah");
     }
 }
